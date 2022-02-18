b207dd214a40afc51d9605b8b7331b42f30f92f6
SCARRLab <88149541+SCARRLab@users.noreply.github.com>
2022-02-18 16:59:47 -0500

    Add files via upload

---
+++ files/startup.m
@@ -0,0 +1,14 @@
+% set graphics root to look better
+
+%% Lines 
+set(groot, 'defaultlinelinewidth', 2); % increase line width 
+set(groot, 'defaultlinemarkersize', 30); % increase marker size 
+set(groot, 'defaulterrorbarlinewidth', 2); % increase line width 
+set(groot, 'defaultrectanglelinewidth', 2); % increase line width
+
+%% Axes 
+set(groot, 'defaultaxesfontsize', 16); % increase axis font size 
+set(groot, 'defaultaxeslinewidth', 2); % increase axis line width 
+set(groot, 'defaultaxesbox', 'off'); % box off 
+set(groot, 'defaultaxesfontname', 'times new roman'); % fonts 
+set(groot, 'defaulttextfontname', 'times new roman'); %
